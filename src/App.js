ba72e5f834a4efb4ff8f1b5a2eb6a8fbee925c2c
Vinni Hoke <vinni.hoke@gmail.com>
2020-01-29 16:35:58 -0800

    Testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  <h1>Welcome to Zeit KP!</h1>
+  return (
+    <h1>Welcome to Zeit KP!</h1>
+  )
 }
 
 export default App;
